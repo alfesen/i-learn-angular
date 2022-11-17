f22b93659b735fda4b71cb13385aa3db3c908d93
alfesen <89510380+alfesen@users.noreply.github.com>
2022-11-17 19:41:01 +0100

    Create list of heroes + styling + editing hero's name

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'John Owen' },
+  { id: 2, name: 'William Gurnall' },
+  { id: 3, name: 'Thomas Watson' },
+  { id: 4, name: 'William Perkins' },
+  { id: 5, name: 'John Bunyan' },
+  { id: 6, name: 'Thomas Brooks' },
+  { id: 7, name: 'John Flavel' },
+  { id: 8, name: 'John Milton' },
+  { id: 9, name: 'Cotton Mather' },
+  { id: 10, name: 'Richard Sibbes' },
+  { id: 11, name: 'Stephen Charnock' },
+  { id: 12, name: 'William Bates' },
+  { id: 13, name: 'Richard Baxter' },
+  { id: 14, name: 'William Bridge' },
+  { id: 15, name: 'Jonathan Edwards' },
+];
