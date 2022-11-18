0cbde4cb9e77cf9f92fb0814b28e38662c7ec8df
alfesen <89510380+alfesen@users.noreply.github.com>
2022-11-18 17:01:37 +0100

    Add routing, edit styles for routed components

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,4 +15,10 @@ export class HeroService {
     this.messagesService.add('Hero service: fetched heroes');
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((hero) => hero.id === id)!;
+    this.messagesService.add(`Hero service: fetched Hero id = ${id}`);
+    return of(hero);
+  }
 }
